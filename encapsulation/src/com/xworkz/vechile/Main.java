008080e1703ffa9c8865b20d16c798d443dfab89
Akhila Patil M P <akhilapatilmp@gmail.com>
2025-03-27 08:39:08 +0530

    Encapsulation and Static Blocks

--- encapsulation/src/com/xworkz/vechile/Main.java
+++ encapsulation/src/com/xworkz/vechile/Main.java
@@ -4,11 +4,9 @@ public class Main {
     public static void main(String[] args) {
                 Car car = new Car("Toyota", 80);
 
-                // Accessing private data using getter
                 System.out.println("Car Brand: " + car.getBrand());
                 System.out.println("Car Speed: " + car.getSpeed() + " km/h");
 
-                // Modifying private data using setter
                 car.setSpeed(100);
 
                 car.showCarDetails();
