fac040dc40910bbf20bb73e4849f14b7316d1dbd
Akhila Patil M P <akhilapatilmp@gmail.com>
2025-04-17 08:11:13 +0530

    Interface

--- interfc/src/com/xworkz/interfc/runner/Main.java
+++ interfc/src/com/xworkz/interfc/runner/Main.java
@@ -1,10 +1,16 @@
 package com.xworkz.interfc.runner;
 
 import com.xworkz.interfc.externali.AC;
+import com.xworkz.interfc.internalcl.SmartAC;
 import com.xworkz.interfc.intfc1.Account;
+import com.xworkz.interfc.intfc1.Student;
 
 public class Main {
     public static void main(String[] args) {
+AC intf1=new Student();
+intf1.cool();
+intf1.heat();
+intf1.adjustTemperature();
 
     }
 }
