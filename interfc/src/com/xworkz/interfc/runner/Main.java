d194e21b93c877358d64531d094c9ccaf6269289
Akhila Patil M P <akhilapatilmp@gmail.com>
2025-04-17 09:03:47 +0530

    Interface

--- interfc/src/com/xworkz/interfc/runner/Main.java
+++ interfc/src/com/xworkz/interfc/runner/Main.java
@@ -1,16 +1,89 @@
 package com.xworkz.interfc.runner;
 
-import com.xworkz.interfc.externali.AC;
-import com.xworkz.interfc.internalcl.SmartAC;
-import com.xworkz.interfc.intfc1.Account;
-import com.xworkz.interfc.intfc1.Student;
+import com.xworkz.interfc.externali.*;
+import com.xworkz.interfc.intfc1.*;
 
 public class Main {
     public static void main(String[] args) {
-AC intf1=new Student();
-intf1.cool();
-intf1.heat();
-intf1.adjustTemperature();
+        Bed bed=new Account();
+        Bicycle bicycle=new Account();
+
+        BicycleHelmet bicycleHelmet=new Album();
+        Blender blender=new Album();
+
+        Blender blender1=new Animal();
+        Boat boat=new Animal();
+
+        BusStop busStop=new Bank();
+        Camera camera=new Bank();
+
+        CarCharger carCharger=new Bike();
+        Chair chair=new Bike();
+
+        Computer computer=new Book();
+        Curtain curtain=new Book();
+
+        Dehumidifier dehumidifier=new Car();
+        Dishwasher dishwasher=new Car();
+
+        Drone drone=new Cat();
+        ElectricCar electricCar=new Cat();
+
+        ElectricKettle electricKettle=new Customer();
+        Elevator elevator=new Customer();
+
+        Elevator elevator1=new Dog();
+        Fan fan=new Dog();
+
+        FanCooler fanCooler=new Employee();
+        FanHeater fanHeater=new Employee();
+
+        FireExtinguisher fireExtinguisher=new Flight();
+        FoodProcessor foodProcessor=new Flight();
+
+        Fridge fridge=new Game();
+        Generator generator=new Game();
+
+        GPSNavigator gpsNavigator=new Hotel();
+        HairDryer hairDryer=new Hotel();
+
+        HomeSecuritySystem homeSecuritySystem=new Invoice();
+        Helicopter helicopter=new Invoice();
+
+        Humidifier humidifier=new Library();
+        HybridCar hybridCar=new Library();
+
+        Iron iron=new Movie();
+        Lamp lamp=new Movie();
+
+        Oven oven=new Music();
+        Phone phone=new Music();
+
+        Printer printer=new Order();
+        Refrigerator refrigerator=new Order();
+
+        SmartSpeaker smartSpeaker=new Player();
+        Speaker speaker=new Player();
+
+        SmokeDetector smokeDetector=new Product();
+        SnowConeMachine snowConeMachine=new Product();
+
+        Sofa sofa=new Room();
+        Table table=new Room();
+
+        AC ac=new Student();
+        Airplane airplane=new Student();
+
+        AirPurifier airPurifier=new Teacher();
+        BarcodeScanner barcodeScanner=new Teacher();
+
+        Train train=new Ticket();
+        Truck truck=new Ticket();
+
+
+
+
+
 
     }
 }
